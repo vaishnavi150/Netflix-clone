e7e440ddcc3ef407f984b871aab74f2a0d9ac280
vaishnavi150 <102913677+vaishnavi150@users.noreply.github.com>
2024-11-08 19:47:55 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -3,8 +3,7 @@ const Footer = () => {
 		<footer className='py-6 md:px-8 md:py-0 bg-black text-white border-t border-gray-800'>
 			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
-					Built by{" "}
-						vaishnavi
+					Built by vaishnavi
 					</a>
 
 				</p>
