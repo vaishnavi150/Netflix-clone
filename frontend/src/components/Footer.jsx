1b4ae04a7f58981d223a630d10a3b2d32a9a190f
vaishnavi150 <102913677+vaishnavi150@users.noreply.github.com>
2024-11-08 19:47:09 +0530

    Update Footer.jsx

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,11 +4,6 @@ const Footer = () => {
 			<div className='flex flex-col items-center justify-between gap-4 md:h-24 md:flex-row'>
 				<p className='text-balance text-center text-sm leading-loose text-muted-foreground md:text-left'>
 					Built by{" "}
-					<a
-						href='https://github.com/burakorkmez'
-						target='_blank'
-						className='font-medium underline underline-offset-4'
-					>
 						vaishnavi
 					</a>
 
